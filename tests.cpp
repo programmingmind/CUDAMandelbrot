5edd89dcd35f7f2d5c1feeebb074fcc3eea8b848
Donovan McKelvey <programmingmind@gmail.com>
2014-03-12 09:10:21 -0700

    add extra debugging information temporarily, add another test function that gives more information

--- tests.cpp
+++ tests.cpp
@@ -4,10 +4,31 @@
 #include <iostream>
 #include <string>
 
-static bool onlyErrors = false;
+#define ALL 0
+#define WARNING 1
+#define ERROR 2
 
+#define MAX_ERROR 0.0000001
+
+static int errorLevel = ALL;
+
+// use this for equality
+template <typename DataType, typename CompareType>
+void printResult(std::string prefix, DataType a, CompareType b, bool exact = true) {
+   if (a == b) {
+      if (errorLevel <= ALL)
+         std::cout << prefix << ": " << "pass" << std::endl;
+   } else if (!exact && (a >= b ? ((a - b) < MAX_ERROR) : ((a - b) > -MAX_ERROR))) {
+      if (errorLevel <= WARNING)
+         std::cout << prefix << ": " << "Warn (diff: " << (a - b) << ") -- " << a << "\t" << b << std::endl;
+   } else {
+      std::cout << prefix << ": " << "!!FAIL!! -- " << a << "\t" << b << std::endl;
+   }
+}
+
+// use this for boolean comparators
 void printResult(std::string prefix, bool b) {
-   if (!b || !onlyErrors)
+   if (!b || errorLevel == ALL)
       std::cout << prefix << ": " << (b ? "pass" : "!!FAIL!!") << std::endl;
 }
 
@@ -15,19 +36,19 @@ int main(int argc, char *argv[]) {
    unsigned int l = 0x56af6f4d;
    unsigned int r = 0x2308ffed;
 
-   onlyErrors = argc > 1;
+   errorLevel = argc > 1 ? atoi(argv[1]) : ALL;
 
    int i = 4;
    Number four(&i, 4);
    Number sixteen = four<<2;
 
-   printResult("partial shift left", four*four == sixteen);
-   printResult("partial shift right", sixteen>>2 == four);
+   printResult("partial shift left", four*four, sixteen);
+   printResult("partial shift right", sixteen>>2, four);
 
-   printResult("integer addition", four+12 == sixteen);
-   printResult("integer subtraction", sixteen - 12 == four);
-   printResult("integer multiplication", four* ((uint32_t)4) == sixteen);
-   printResult("integer division", sixteen / 4 == four);
+   printResult("integer addition", four + 12, sixteen);
+   printResult("integer subtraction", sixteen - 12, four);
+   printResult("integer multiplication", four * 4U, sixteen);
+   printResult("integer division", sixteen / 4, four);
 
    Number a(4);
    Number b(4);
@@ -44,28 +65,28 @@ int main(int argc, char *argv[]) {
 
    uint32_t *bigData = (uint32_t *) ((d<<32) | r).getData();
 
-   printResult("left shift, or 1", bigData[0] == r);
-   printResult("left shift, or 2", bigData[1] == l);
+   printResult("left shift, or 1", bigData[0], r);
+   printResult("left shift, or 2", bigData[1], l);
    free(bigData);
 
    Number n(&l, 4);
    n += b;
    uint32_t *nData = (uint32_t *) n.getData();
-   printResult("+=", *nData == (l+r));
-   printResult("+= == +", n == (a + b));
+   printResult("+=", *nData, (l+r));
+   printResult("+= == +", n, (a + b));
    free(nData);
 
    uint32_t *cData = (uint32_t *) c.getData(), *aData = (uint32_t *) a.getData();
 
-   printResult("constructor from pointer", *cData == *aData);
+   printResult("constructor from pointer", *cData, *aData);
    free(cData);
    free(aData);
 
    uint32_t *sum = (uint32_t *) (a+b).getData();
    uint32_t *diff = (uint32_t *) (a-b).getData();
 
-   printResult("sum", *sum == (l+r));
-   printResult("diff", *diff == (l-r));
+   printResult("sum", *sum, (l+r));
+   printResult("diff", *diff, (l-r));
 
    free(sum);
    free(diff);
@@ -80,7 +101,7 @@ int main(int argc, char *argv[]) {
 
    uint64_t prod = 0xbdd085c01afbd49ULL;
    uint64_t *prodData = (uint64_t *) (a*b).getData();
-   printResult("prod", *prodData == prod);
+   printResult("prod", *prodData, prod);
 
    free(prodData);
 
@@ -89,36 +110,43 @@ int main(int argc, char *argv[]) {
    Number nFortyEight(4);
    nFortyEight = 48U;
 
-   printResult("division #1", (a*b)/a == b);
-   printResult("division #2", (a*b)/b == a);
-   printResult("division #3", (nThree / nThree) == 1U);
-   printResult("division #4", (nFortyEight / nThree) == 16U);
+   std::cout<<(nThree << 4)<<std::endl;
+   std::cout<<(nThree << 64)<<std::endl;
+   std::cout<<(nThree << 32)<<std::endl;
+   std::cout<<(nThree << 68)<<std::endl;
+
+   std::cout<<(nThree << 68)/480U<<std::endl;
+
+   printResult("division #1", (a*b)/a, b);
+   printResult("division #2", (a*b)/b, a);
+   printResult("division #3", (nThree / nThree), 1U);
+   printResult("division #4", (nFortyEight / nThree), 16U);
 
    uint64_t prod16 = 0x56af6f4d0ULL;
    Number n16(&prod16, 8);
 
    uint64_t *shiftData = (uint64_t *) (a<<4).getData();
-   printResult("32 bit value << partial byte", *shiftData == prod16);
+   printResult("32 bit value << partial byte", *shiftData, prod16);
    free(shiftData);
 
    uint32_t *rShiftData = (uint32_t *) (n16>>4).getData();
-   printResult("shift it back right", *rShiftData == l);
+   printResult("shift it back right", *rShiftData, l);
    free(rShiftData);
 
    rShiftData = (uint32_t *) (n16>>12).getData();
-   printResult("shift right over a byte", *rShiftData == (l >> 8));
+   printResult("shift right over a byte", *rShiftData, (l >> 8));
    free(rShiftData);
 
    uint64_t *prod16Data = (uint64_t *) (a*sixteen).getData();
-   printResult("prod16", *prod16Data == prod16);
+   printResult("prod16", *prod16Data, prod16);
    free(prod16Data);
 
    prod16Data = (uint64_t *) (a*((uint32_t)16)).getData();
-   printResult("prod16 int", *prod16Data == prod16);
+   printResult("prod16 int", *prod16Data, prod16);
    free(prod16Data);
 
-   printResult("division by power of 2", sixteen/four == four);
-   printResult("compound multiplication, division by power of 2", (a*sixteen)/four == a*four);
+   printResult("division by power of 2", sixteen/four, four);
+   printResult("compound multiplication, division by power of 2", (a*sixteen)/four, a*four);
 
    // Mersenne prime for modulus M_89 = 1<<89 - 1
    uint32_t zero = 0;
@@ -136,12 +164,12 @@ int main(int argc, char *argv[]) {
    mod += 1024;
    printResult("modulus without remainder", mod % 32 == 0);
 
-   printResult("float vs double constructor 1", Decimal(5.0) == Decimal(5.0f));
-   printResult("float vs double constructor 2", Decimal(7.25) == Decimal(7.25f));
-   printResult("float vs double constructor 3", Decimal(0.0) == Decimal(0.0f));
-   printResult("float vs double constructor 4", Decimal(-1.375) == Decimal(-1.375f));
+   printResult("float vs double constructor 1", Decimal(5.0), Decimal(5.0f));
+   printResult("float vs double constructor 2", Decimal(7.25), Decimal(7.25f));
+   printResult("float vs double constructor 3", Decimal(0.0), Decimal(0.0f));
+   printResult("float vs double constructor 4", Decimal(-1.375), Decimal(-1.375f));
 
-   printResult("Number vs double constructor", Decimal(5.0) == Decimal(5U));
+   printResult("Number vs double constructor", Decimal(5.0), Decimal(5U));
 
    printResult("constructor with 0", Decimal(0.0) == 0U);
 
@@ -154,18 +182,18 @@ int main(int argc, char *argv[]) {
    std::cout<<"twelve : " << dTwelve << std::endl;
    std::cout<<"sixteen: " << dSixteen << std::endl;*/
 
-   printResult("decimal addition", dFour + dTwelve == dSixteen);
-   printResult("decimal subtraction", dSixteen - dTwelve == dFour);
-   printResult("decimal multiplication", dFour * dFour == dSixteen);
-   printResult("decimal division", dSixteen / dFour == dFour);
-   printResult("decimal division 2", Decimal(48U) / Decimal(3.0) == dSixteen);
+   printResult("decimal addition", dFour + dTwelve, dSixteen);
+   printResult("decimal subtraction", dSixteen - dTwelve, dFour);
+   printResult("decimal multiplication", dFour * dFour, dSixteen);
+   printResult("decimal division", dSixteen / dFour, dFour);
+   printResult("decimal division 2", Decimal(48U) / Decimal(3.0), dSixteen);
 
    printResult("decimal greater than", dTwelve > dFour);
    printResult("decimal less than", dTwelve < dSixteen);
 
    Decimal dAssign(0.0);
    dAssign = dFour;
-   printResult("decimal assign", dAssign == dFour);
+   printResult("decimal assign", dAssign, dFour);
 
    Decimal dNegThree(-3.0);
    Decimal dNegFour(-4.0);
@@ -177,10 +205,10 @@ int main(int argc, char *argv[]) {
    printResult("decimal greater than zero", dSixteen > dZero);
    printResult("decimal neg less than zero", dNegSixteen < dZero);
 
-   printResult("decimal neg addition", dSixteen + dNegFour == dTwelve);
-   printResult("decimal neg subtraction", dFour - dNegTwelve == dSixteen);
-   printResult("decimal neg multiplcation", dFour * dNegTwelve == dNegFortyEight);
-   printResult("decimal neg division", dNegFortyEight / dNegThree == dSixteen);
+   printResult("decimal neg addition", dSixteen + dNegFour, dTwelve);
+   printResult("decimal neg subtraction", dFour - dNegTwelve, dSixteen);
+   printResult("decimal neg multiplcation", dFour * dNegTwelve, dNegFortyEight);
+   printResult("decimal neg division", dNegFortyEight / dNegThree, dSixteen);
 
    printResult("decimal neg greater than", dNegTwelve > dNegSixteen);
    printResult("decimal neg less than", dNegSixteen < dNegFour);
@@ -190,16 +218,25 @@ int main(int argc, char *argv[]) {
    Decimal startY = -1.00;
    Decimal resolution = INITIAL_RESOLUTION;
 
-   unsigned int yNdx, xNdx;
-   for (unsigned int xNdx = 0; xNdx < WIDTH; xNdx++) {
-      for (unsigned int yNdx = 0; yNdx < HEIGHT; yNdx++) {
+   std::cout<<startX<<"\t"<<startY<<std::endl;
+
+   std::cout<<"resolution: " << resolution<<std::endl;
+   std::cout<<resolution/WIDTH<<std::endl;
+   std::cout<<(resolution*2U)/WIDTH<<std::endl;
+   std::cout<<startX + resolution/WIDTH<<std::endl;
+
+   for (unsigned int xNdx = 2; xNdx < 3; xNdx++) {
+      for (unsigned int yNdx = 2; yNdx < 3; yNdx++) {
          Decimal x0(0U), y0(0U);
 
          x0 = startX + ((resolution * xNdx) / WIDTH);
          y0 = startY + ((resolution * yNdx) / HEIGHT);
 
-         printResult("x0", x0 == -1.50 + ((INITIAL_RESOLUTION * xNdx) / WIDTH));
-         printResult("y0", y0 == -1.00 + ((INITIAL_RESOLUTION * yNdx) / HEIGHT));
+         printResult("x0", x0, Decimal(-1.50 + ((INITIAL_RESOLUTION * xNdx) / WIDTH)));
+         printResult("y0", y0, Decimal(-1.00 + ((INITIAL_RESOLUTION * yNdx) / HEIGHT)));
+
+         std::cout << x0 << "\t" << (-1.50 + ((INITIAL_RESOLUTION * xNdx) / WIDTH)) << std::endl;
+         std::cout << y0 << "\t" << (-1.00 + ((INITIAL_RESOLUTION * yNdx) / HEIGHT)) << std::endl;
       }
    }  
 
